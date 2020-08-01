a0c694d4a8e03d2c1eedc00918776957fe95f4fd
BradD <bradleyeday@gmail.com>
2020-08-01 14:56:06 -0500

    cannot get program to start...

--- Index.js
+++ Index.js
@@ -1,7 +1,22 @@
 const inquirer = require("inquirer");
-const Question = require('./lib/Question')
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer')
+const Employee = require('./lib/Employee')
+
+function Staff() {
+    this.employeeNumber = 0;
+    this.employees = [];
+
+    Staff.prototype.generateStaff = function() {
+        this.employeeNumber++
+        if (this.employeeNumber = 1) {
+            const employee = new Employee()
+            // not sure how to get this to work..
+        }
+    }
+}
 
 
-
-
+new Staff().generateStaff();
 
