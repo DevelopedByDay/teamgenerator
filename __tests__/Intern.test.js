c2b9de8b5a950b38341e8761ba748b3e498e599c
BradD <bradleyeday@gmail.com>
2020-08-01 14:49:36 -0500

    all staff tests functioning

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,9 @@
 const Employee = require('../lib/Employee')
 const Intern = require('../lib/Intern')
+
+test('tests Intern object functionality', () => {
+    const intern = new Intern('Joey')
+
+    expect(intern.role).toBe('Intern')
+    expect(intern.school).toBe('')
+});
